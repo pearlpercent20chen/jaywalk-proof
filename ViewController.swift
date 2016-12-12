16ad19c29193cde6654536dcfda7c71b7c08459c
pearlpercent20chen <pearl.chen@student.tdsb.on.ca>
2016-12-12 17:50:24 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  jaywalk free
+//
+//  Created by Pearl Chen on 2016-12-12.
+//  Copyright © 2016 Pearl Chen. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        self.locationManager.stopUpdatingLocation()
+        
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didErrorHappen error: NSError) {
+        print("Oops: " + error.localizedDescription)
+    }
+
+
+}
+
